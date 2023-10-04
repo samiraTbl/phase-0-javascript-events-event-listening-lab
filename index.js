a49a820ea76d8637b8e28019712d2663d4e6c70e
samira toubal seghir <samsidt@gmail.com>
2023-10-04 16:45:53 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const input = document.getElementById('button');
-console.log(input);
-input.addEventListener('click', function() {
+
+function clickAlert() {
   alert('I was clicked!');
-});
+}
+
+input.addEventListener('click', clickAlert);
